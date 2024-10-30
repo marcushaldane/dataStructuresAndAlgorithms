533192ff00dc7ea5d445376cb063331b84bbcd46
marcushaldane <mrhaldane2017@gmail.com>
2024-10-30 14:08:45 -0500

    changed printLinkedList function to use dot operator and added printLinkedListArrowNotation function which uses arrow notation and slightly different style for printing a linked list.

--- LinkedLists/linkedList.c
+++ LinkedLists/linkedList.c
@@ -7,29 +7,39 @@ struct node {
 };
 
 void printLinkedList(struct node* head);
+void printLinkedListArrowNotation(struct node* head);
 
 int main () {
 
     printf("==========================================\n");
-    struct node* head; 
-    struct node n1 = {1, NULL};
-    struct node n2 = {2, NULL};
-    struct node n3 = {3, NULL};
-    head = &n1;
-    n1.next = &n2;
+    struct node* head; // node pointer to head of linked list
+    struct node n1 = {1, NULL}; // node n1
+    struct node n2 = {2, NULL}; // node n2
+    struct node n3 = {3, NULL}; // node n3
+    head = &n1; // head pointer is assigned to the address of n1
+    n1.next = &n2; 
     n2.next = &n3;
-    printLinkedList(head);
+    printLinkedListArrowNotation(head);
     printf("==========================================\n");
-    
     return 0;
 }
 
 void printLinkedList(struct node* head) {
     struct node* curr = head;
     printf("[ ");
+    while((*curr).next != NULL) {
+        printf("%d, ", (*curr).value);
+        curr = (*curr).next;
+    }
+    printf("%d ]\n", (*curr).value);
+}
+
+void printLinkedListArrowNotation(struct node* head) {
+    struct node* curr = head;
     while(curr->next != NULL) {
-        printf("%d, ", curr->value);
+        printf("[%d]->", curr->value);
         curr = curr->next;
     }
-    printf("%d ]\n", curr->value);
+    printf("[%d]\n", curr->value);
 }
+
